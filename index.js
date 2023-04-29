de338037d4205b4ab9000150b828f638e43b8928
zhusophia <iceyes123@gmail.com>
2023-04-29 16:39:24 -0400

    fnished title transition, starting main sophland

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function displaydiv(idname, replacename) {
     var div = document.getElementById(idname)
     var replace = document.getElementById(replacename)
-  //  let fade1 = document.getElementById(fadename);
     replace.style.display = 'none';
-   // fade1.classList.toggle('fade');
     div.style.display = 'block';
 }
 
