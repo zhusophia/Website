cf4da18a556bb40bdf4848149bada8cace08b785
zhusophia <iceyes123@gmail.com>
2023-05-02 16:18:49 -0400

    intro transition when loading page

--- home.js
+++ home.js
@@ -8,12 +8,12 @@ var count = 0;
 
 var tippossible = [
     "TIP: Keep your hands and feet inside SophLand rides to mitigate the probability of meteor strikes.",
-   // "TIP: SophLand is legally responsible for your safety. Sue us!",
-   // "TIP: SophLand Sprite is 7.2 milion dollars. A reasonable price, no?",
-  //  "FACT: SophLand averages 4.6 trillion monthly visitors!",
- //   "FACT: SophLand has an 100% satisfaction rate (Based off: 1 review by Sophia Zhu).",
-  //  "FACT: SophLand has a population of at least 0.",
-  //  "FACT: Github servers are currently online!"
+    "TIP: SophLand is legally responsible for your safety. Sue us!",
+    "TIP: SophLand Sprite is 7.2 milion dollars. A reasonable price, no?",
+    "FACT: SophLand averages 4.6 trillion monthly visitors!",
+    "FACT: SophLand has an 100% satisfaction rate (Based off: 1 review by Sophia Zhu).",
+    "FACT: SophLand has a population of at least 0.",
+    "FACT: Github servers are currently online!"
 ];
 
 var randomNumber = Math.floor(Math.random() * tippossible.length);
@@ -30,7 +30,6 @@ hamburger.addEventListener("click", () => {
         check = true;
     } else if (!check) {
         aniwrapper.style.visibility="active";
-      //  wrapper.style.animation="fadein 0.4s linear";
         wrapper.classList.remove("fade");
         hamburger.classList.add("active");
         wrapper.classList.add("active");
