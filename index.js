c895559130549905b22d4b880e0d44207508aa61
zhusophia <iceyes123@gmail.com>
2023-05-27 16:56:13 -0400

    added index page hint and other qol improvements

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
+var count1 = 0
+var count2 = 0
+
 function displaydiv(idname, replacename) {
     var div = document.getElementById(idname)
     var replace = document.getElementById(replacename)
     replace.style.display = 'none';
     div.style.display = 'block';
+    count1++;
 }
 
-function removediv(divname) {
-    var remove = document.getElementById(divname)
-    remove.style.opacity = 0;
+function removediv() {
+    document.getElementById('stuck').style.display = 'none'
+    var remove = document.getElementById('hello');
+    remove.style.display = 'none';
+    setInterval(warning2, 6000)
 }
+
+function hint() {
+    document.getElementById('stuck').style.display = 'block'
+}
+
+setInterval(warning1, 6000)
+
+function warning1() {
+    if (count1 == 0) {
+        hint();
+    } 
+}
+
+
+function warning2() {
+    if (count2 == 0) {
+       hint();
+    } 
+}
+
+  
